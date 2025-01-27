444f369dfc4ee7bcde7e9c18ab351c23e63aece2
johnssonviggo <viggo.johnsson1@gmail.com>
2025-01-27 10:16:36 +0100

    bilder och html fungerar!

--- lib/tcp_server.rb
+++ lib/tcp_server.rb
@@ -16,9 +16,7 @@ class HTTPServer
         puts "Listening on #{@port}"
         router = Router.new
         router.add_route(:get, "/banan") do
-            "<h1>#{1 + 2}BANAN</h1>
-            <img src='/img/bird-with-arms.jpg'>
-            <img src='/img/raft.png'>"
+            File.read("views/index.html")
         end
         router.add_route(:get, "/senap") do
             "<h1>SENAP</h1>"
@@ -37,35 +35,87 @@ class HTTPServer
 
             request = Request.new(data)
             # pp request
-            result = router.match_route(request)
+            matching_route = router.match_route(request)
+            # if matching_route
+            #     response = Response.new(matching_route, session)
+            # elsif #kolla om filen finns i public
+            #     filen = ....
+            #     response = Response.new(filen, session)
+            # else
+            #     data = "Oh noes"
+            #     response = Response.new(data, session, 404)
+            # end
 
-            file_path = File.join(PUBLIC_DIR, request.resource)
-
-            if result
-                status = 200
-                content_type = "text/html"
-                content = result[:block].call
-                p request
-                p "här"
-            elsif File.exist?(file_path)
-                status = 200
-                content_type = MIME::Types.type_for(file_path).first.to_s || "application/octet-stream"
-                content = File.binread(file_path)
+            if matching_route
+                response = Response.new(matching_route[:block].call, session)
             else
-              status = 404
-              content_type = "text/html"
-              content = "<h1>No found</h1>"
+                file_path = File.join(PUBLIC_DIR, request.resource)
+            if File.exist?(file_path) && File.file?(file_path)
+                    response = Response.new(file_path, session, file: true)
+            else
+                    #404 Not found
+                    response = Response.new("Oh noes", session, status: 404)
             end
+        end
 
 
-            session.print "HTTP/1.1 #{status}\r\n"
-            session.print "Content-Type: #{content_type}\r\n"
-            session.print "Content-Length: #{content.bytesize}\r\n"
-            session.print "\r\n"
-            session.print content
-            session.close
+            response.send
+
         end
     end
+end
+
+class Response
+
+    def initialize(result, session, file: false, status: 200)
+        @session = session
+        @result = result
+        @status = status
+        @file = file
+    end
+
+
+    def send
+        # if @result
+        #     content_type = "text/html"
+        #     content = @result[:block].call
+
+        # elsif File.exist?(file_path)
+        #     file_path = File.join(PUBLIC_DIR, request.resource)
+        #     content_type = MIME::Types.type_for(file_path).first.to_s || "application/octet-stream"
+        #     content = File.binread(file_path)
+        # else
+        #   content_type = "text/html"
+        #   content = "<h1>No found</h1>"
+        # end
+
+        if @file
+            content_type = MIME::Types.type_for(@result).first.to_s || "application/octet-stream"
+            content = File.binread(@result)
+        else
+          content_type = "text/html"
+          content = @result
+        end
+
+
+        @session.print "HTTP/1.1 #{@status}\r\n"
+        @session.print "Content-Type: #{content_type}\r\n"
+        @session.print "Content-Length: #{content.bytesize}\r\n"
+        @session.print "\r\n"
+        @session.print content
+        @session.close
+    end
+
+    private
+
+  def status_message
+    case @status
+    when 200 then "OK"
+    when 404 then "Not Found"
+    when 500 then "Internal Server Error"
+    else "Unknown"
+    end
+  end
 end
 
 server = HTTPServer.new(4567)
